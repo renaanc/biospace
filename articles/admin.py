8c9bd8bc8587f7e5f746870ae922d18830fdee46
renaanc <renaanc95@gmail.com>
2025-05-13 20:16:07 -0300

    article subtitle  update

--- articles/admin.py
+++ articles/admin.py
@@ -4,6 +4,10 @@ from .models import Article
 
 @admin.register(Article)
 class ArticleAdmin(TranslatableAdmin):
-    list_display = ('title', 'slug', 'created_at', 'image_caption')
+    list_display = ('title', 'slug', 'created_at', 'get_image_caption')
     search_fields = ('translations__title', 'translations__content')
     ordering = ('-created_at',)
+
+    def get_image_caption(self, obj):
+        return obj.image_caption
+    get_image_caption.short_description = 'Legenda'
