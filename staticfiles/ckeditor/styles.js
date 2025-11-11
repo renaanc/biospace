d57bc882ab574ff45cb230445c839ddfe228f746
renaanc <renaanc95@gmail.com>
2025-11-10 23:18:32 -0300

    att logo3

---
+++ staticfiles/ckeditor/styles.js
@@ -0,0 +1,11 @@
+CKEDITOR.stylesSet.add('default', [
+    // Negrito
+    { name: 'Negrito', element: 'strong' },
+
+    // Itálico
+    { name: 'Itálico', element: 'em' },
+
+    //Paragrafo
+    { name: 'Parágrafo Recuado', element: 'p', attributes: { 'style': 'text-indent:2em;'  } },
+
+]);
