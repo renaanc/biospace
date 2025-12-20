fa31433f08422fd8c3934e101f527db304da3387
renaanc <renaanc95@gmail.com>
2025-12-19 23:17:00 -0300

    update urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -8,9 +8,5 @@ app_name = 'articles'  # Aqui definimos o namespace
 urlpatterns = [
     path('articles/', views.article_list, name='article_list'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
-]
-
-
-urlpatterns = [
     path("like/<slug:slug>/", like_article, name="like_article"),
 ]
